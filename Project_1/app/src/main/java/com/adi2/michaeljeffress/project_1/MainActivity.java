fec13acacf97133b6b08274bf85b463b140ae816
Michael Jeffress <michaeljeffress@Michaels-MacBook-Pro.local>
2016-06-23 15:57:55 -0700

    Basic App working

--- Project_1/app/src/main/java/com/adi2/michaeljeffress/project_1/MainActivity.java
+++ Project_1/app/src/main/java/com/adi2/michaeljeffress/project_1/MainActivity.java
@@ -17,10 +17,8 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton;
     private ListView mListView;
     private EditText mEditText;
-    private Button mButtonRemove;
     private ArrayList<String> mArrayList = new ArrayList<String>();
     private ArrayAdapter<Category> mArrayAdapter;
-    private ArrayList<ArrayList<String>> m2ArrayList;
     private ArrayList<Category> categories;
 
     public static int BUTTON_REQUEST_CODE = 27;
@@ -33,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
         mEditText = (EditText)findViewById(R.id.editText_Main);
         mButton = (Button)findViewById(R.id.button_Main);
-        mButtonRemove = (Button)findViewById(R.id.button_RemoveToDo);
         categories = new ArrayList<>();
 
         mArrayAdapter = new ArrayAdapter<Category>(this,android.R.layout.simple_list_item_1,categories);
@@ -46,19 +43,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String toDo = mEditText.getText().toString();
                 mArrayList.add(toDo);
+                Category mCategory = new Category ();
+                mCategory.setCategory(toDo);
+                categories.add(mCategory);
                 mArrayAdapter.notifyDataSetChanged();
                 mEditText.setText("");
-                //Need to stop it from adding black todos
+                //Need to stop it from adding blank todos
             }
         });
 
-        mButtonRemove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mArrayList.remove(mArrayList.size() - 1);
-                mArrayAdapter.notifyDataSetChanged();
-            }
-        });
 
         mListView = (ListView) findViewById(R.id.listView_Main);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -67,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 Category cat = categories.get(position);
                 Intent intentMain = new Intent(MainActivity.this, TasksActivity.class);
                 intentMain.putExtra("name", cat.getCategory());
-                intentMain.putExtra("todos", cat.getTodos());
+                intentMain.putExtra("tasks", cat.getTodos());
                 intentMain.putExtra("pos", position);
                 startActivityForResult(intentMain, LISTVIEW_REQUEST_CODE);
 
@@ -75,16 +68,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                mArrayList.remove(position);
+                return false;
+            }
+        });
+
         }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == LISTVIEW_REQUEST_CODE){
             if(resultCode == RESULT_OK) {
-                ArrayList<String> receivedResults = data.getStringArrayListExtra("todos");
-                int Position = data.getIntExtra("pos", 0);
-                categories.get(Position).setTodos(receivedResults);
+                ArrayList<String> receivedResults = data.getStringArrayListExtra("tasks");
+                int position = data.getIntExtra("pos", -1);
+                Category category = new Category();
+                String name = data.getStringExtra("name");
+                category.setCategory(name);
+                category.setTodos(receivedResults);
+                categories.set(position, category);
                 mArrayAdapter.notifyDataSetChanged();
 
             }
--- Project_1/app/src/main/java/com/adi2/michaeljeffress/project_1/TasksActivity.java
+++ Project_1/app/src/main/java/com/adi2/michaeljeffress/project_1/TasksActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.Element;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,15 +21,15 @@ import java.util.ArrayList;
  */
 public class TasksActivity extends AppCompatActivity {
     private Button tButtonAdd;
-    private Button tButtonRemove;
     private Button tButton_Back;
     private EditText tEditText;
     private ListView tListView;
-    private ArrayList<String> tArrayList = new ArrayList<String>();
+    private ArrayList<String> tArrayList;
     private ArrayAdapter<String> tArrayAdapter;
 
     public static int BUTTON_REQUEST_CODE = 28;
     public static int LISTVIEW_REQUEST_CODE  = 24;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -37,9 +38,15 @@ public class TasksActivity extends AppCompatActivity {
 
         tEditText = (EditText)findViewById(R.id.editText_Tasks);
         tButtonAdd = (Button)findViewById(R.id.button_AddTask);
-        tButtonRemove = (Button)findViewById(R.id.button_RemoveTask);
         tButton_Back = (Button)findViewById(R.id.tButton_Back);
         tListView = (ListView)findViewById(R.id.listView_Tasks);
+
+        Intent recievedIntent = getIntent();
+        tArrayList = recievedIntent.getStringArrayListExtra("tasks");
+        if (tArrayList == null) {
+            tArrayList = new ArrayList<String>();
+        }
+
         tArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,tArrayList);
 
         tListView.setAdapter(tArrayAdapter);
@@ -52,49 +59,45 @@ public class TasksActivity extends AppCompatActivity {
                 tArrayList.add(task);
                 tArrayAdapter.notifyDataSetChanged();
                 tEditText.setText("");
-                //Need to stop it from adding black todos
+                //Need to stop it from adding blank tasks
             }
         });
 
-        tButtonRemove.setOnClickListener(new View.OnClickListener() {
+
+
+        tListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onClick(View v) {
-                tArrayList.remove(tArrayList.size() - 1);
-     //           tArrayList.remove(position);
-                tArrayAdapter.notifyDataSetChanged();
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                tArrayList.remove(position);
+                return false;
             }
         });
-
-        Intent recievedIntent = getIntent();
-        String  intentTask = recievedIntent.getStringExtra("todo");
 
         tButton_Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String tasks = tEditText.getText().toString();
                 Intent resultIntent = getIntent();
-                resultIntent.putExtra("todos", tasks);
+                resultIntent.putExtra("tasks", tArrayList);
                 setResult(RESULT_OK, resultIntent);
                 finish();
 
             }
         });
 
-//            @Override
-//            public void onClick(View v) {
-//                String editText_Task = tEditText.getText().toString();
-//                Intent intent = new Intent(TasksActivity.this, MainActivity.class);
-//                intent.putExtra("Task", editText_Task);
-//                //Bundle bundle = new Bundle();
-//                setResult(RESULT_OK, intent);
-//                startActivity(intent);
-//                finish();
-//
-//            }
-
-
-
 
     }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if(requestCode == LISTVIEW_REQUEST_CODE){
+//            if(resultCode == RESULT_OK) {
+//                ArrayList<String> receivedResults = data.getStringArrayListExtra("todos");
+//                int Position = data.getIntExtra("pos", 0);
+//                categories.get(Position).setTodos(receivedResults);
+//                tArrayAdapter.notifyDataSetChanged();
+//
+//            }
+//
+//        }
+//    }
 
 }
